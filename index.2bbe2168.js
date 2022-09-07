7f853dcff02f0210055d99fe05dc91312f5ec780
KateDietkova <38350154+KateDietkova@users.noreply.github.com>
2022-09-07 16:17:42 +0000

    Deploying to gh-pages from @ KateDietkova/wow-ice-cream@1e74af15519edfec326e73ec114614f3dcb0d089 🚀

---
+++ index.2bbe2168.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$("body").append('<a href="#" id="go-top" title="UP">UP</a>')})),$((function(){$.fn.scrollToTop=function(){$(this).hide().removeAttr("href"),$(window).scrollTop()>="250"&&$(this).fadeIn("slow");var o=$(this);$(window).scroll((function(){$(window).scrollTop()<="250"?$(o).fadeOut("slow"):$(o).fadeIn("slow")})),$(this).click((function(){$("html, body").animate({scrollTop:0},"slow")}))}})),$((function(){$("#go-top").scrollToTop()}));
+//# sourceMappingURL=index.2bbe2168.js.map
